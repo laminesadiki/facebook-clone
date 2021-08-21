f7bdca2e1cd31c0ae5be39014efe5aa1c94a2d04
sadiki-dev <sadiki.dev@gmail.com>
2021-08-21 12:08:42 +0200

    install next-auth , DO server side rendering and use Facebook API

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import { getSession } from 'next-auth/client'
 import Head from 'next/head'
 import Header from '../components/Header'
 
-export default function Home() {
+export default function Home({session}) {
+  if (!session) return <Login />;
   return (
     <div>
       <Head>
@@ -14,8 +16,16 @@ export default function Home() {
          {/* Feed */}
          {/* Widgets */}
       </main>
-
-
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  //Get the user
+  const session = await getSession(context);
+  return {
+    props: {
+      session
+    }
+  }
 }
